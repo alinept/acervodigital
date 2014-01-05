8eeeb5056940ea7d4268306e37af83d48a8b7f70
alinept <alineporfirot@gmail.com>
2014-01-05 18:12:23 -0300

    excluindo classe binary

--- src/br/ufma/les/acervodigital/window/GerenciarDiretoriosDocumentosWindow.java
+++ src/br/ufma/les/acervodigital/window/GerenciarDiretoriosDocumentosWindow.java
@@ -1,11 +1,18 @@
 package br.ufma.les.acervodigital.window;
 
+import java.sql.SQLException;
+
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
 import org.zkoss.zkplus.databind.DataBinder;
+import org.zkoss.zul.DefaultTreeModel;
+import org.zkoss.zul.TreeModel;
+import org.zkoss.zul.TreeNode;
 import org.zkoss.zul.Window;
 
 import br.ufma.les.acervodigital.fachada.AcervoDigitalFachada;
 import br.ufma.les.acervodigital.fachada.AcervoDigitalFachadaImpl;
+import br.ufma.les.acervodigital.treemodel.PackageData;
+import br.ufma.les.acervodigital.treemodel.PackageDataUtil;
 
 public class GerenciarDiretoriosDocumentosWindow extends Window{
 	
@@ -14,6 +21,7 @@ private static final long serialVersionUID = 1L;
 	public Window window;
 	private DataBinder binder;
 	private AcervoDigitalFachada acervoDigitalFachada;
+	private TreeModel<TreeNode<PackageData>> arvore;
 	
 	public void onCreate()
     {
@@ -21,7 +29,28 @@ private static final long serialVersionUID = 1L;
         binder =  new AnnotateDataBinder(window);
         binder.loadAll();
         acervoDigitalFachada = new AcervoDigitalFachadaImpl();
-       
+        PackageDataUtil pk = new PackageDataUtil();
+        try {
+			pk.montaArvore(acervoDigitalFachada.retornaCaminhoDiretorioRaiz());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	arvore = new DefaultTreeModel<PackageData>(pk.getRoot());
+
+        binder.loadAll();
     }
+
+	public TreeModel<TreeNode<PackageData>> getArvore() {
+		return arvore;
+	}
+
+	public void setArvore(TreeModel<TreeNode<PackageData>> arvore) {
+		this.arvore = arvore;
+	}
+	
+	
 
 }
